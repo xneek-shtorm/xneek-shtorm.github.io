8dd3fa8deea5d43ab3c9669d4e7f89c26530771c
Nikolay Fedoseev <n.fedoseev@whyhappen.ru>
2026-02-02 15:46:47 +0300

    update baristas

--- data/make.js
+++ data/make.js
@@ -11,8 +11,33 @@ fs.readFile('./barista-array.json', 'utf8', (err, data) => {
     jsonData.forEach((b) => {
       try {
         const filePath = path.resolve(`./barista/${b.id}.json`);
+        b.addresses = [
+          {
+            channel: 'email',
+            address: `barista-${b.id}@barista.ru`
+          },
+          {
+            channel: 'phone',
+            address: `7${`${b.id}`.padEnd(10,b.id.toString())}`
+          },
+          {
+            channel: 'tg',
+            address: `${`${b.id}`.padEnd(6,b.id.toString())}`
+          },
+          {
+            channel: 'chat',
+            address: crypto.randomUUID()
+          }
+        ].slice(0,Math.ceil(Math.random()*5));
+
         fs.writeFileSync(filePath, JSON.stringify(b, null, 2), 'utf8');
         console.log(`JSON file "${filePath}" created successfully.`);
+
+
+        const filePath1 = path.resolve(`./barista-search-one/${b.id}.json`);
+        fs.writeFileSync(filePath1, JSON.stringify({results: [b]}, null, 2), 'utf8');
+        console.log(`JSON file "${filePath1}" created successfully.`);
+
       } catch (err) {
         console.error('Error writing JSON file:', err);
       }
